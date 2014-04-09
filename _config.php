31228379016889fff90748903b062d6936a0dbb9
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-04-09 04:40:41 +0000

    UPGRADE 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/dataintegritytests/trunk@5588 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -4,11 +4,6 @@
 /**
  *
  * @author: Nicolaas - modules [at] sunnysideup.co.nz
- * @help: // to see in action, add _configs per below, and go to http://www.mysite.com/dev/tasks/DataIntegrityTest/
  **/
 
 
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
-//===================---------------- START dataintegritytests MODULE ----------------===================
-
-//===================---------------- END dataintegritytests MODULE ----------------===================
