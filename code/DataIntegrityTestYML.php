c62b54fb1b2693aece905fc37e3c8a960074e603
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-06-27 00:21:24 +0000

    ENHANCEMENTS
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/dataintegritytests/trunk@5725 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/DataIntegrityTestYML.php
+++ code/DataIntegrityTestYML.php
@@ -11,15 +11,16 @@ class DataIntegrityTestYML extends BuildTask {
 
 	/**
 	 * list of classes that do not need to be checked
+	 * NB: they are all lowercase, as we test for them only lowercase!
 	 * @var array
 	 */
-	private static $classes_to_skip = array("Name", "Before", "Only", "After");
+	private static $classes_to_skip = array("name", "before", "only", "after");
 
 	/**
 	 * list of variables that do not need checking...
 	 * @var array
 	 */
-	private static $variables_to_skip = array("extensions", "icon");
+	private static $variables_to_skip = array();
 
 	/**
 	 * standard SS variable
@@ -40,6 +41,7 @@ class DataIntegrityTestYML extends BuildTask {
 		$classesToSkip = Config::inst()->get("DataIntegrityTestYML", "classes_to_skip");
 		$variablesToSkip = Config::inst()->get("DataIntegrityTestYML", "variables_to_skip");
 		foreach($filesArray as $folderAndFileLocation){
+			db::alteration_message("<h2>Checking $folderAndFileLocation</h2>");
 			$fixtureFolderAndFile = Director::baseFolder().'/'. $folderAndFileLocation;
 			if(!file_exists($fixtureFolderAndFile)) {
 				user_error('No custom configuration has been setup here : "' . $fixtureFolderAndFile . '" set the files here: DataIntegrityTestYML::config_files', E_USER_NOTICE);
@@ -47,7 +49,7 @@ class DataIntegrityTestYML extends BuildTask {
 			$parser = new Spyc();
 			$arrayOfSettings = $parser->loadFile($fixtureFolderAndFile);
 			foreach($arrayOfSettings as $className => $variables) {
-				if(in_array($className, $classesToSkip )) {
+				if(in_array(strtolower($className), $classesToSkip )) {
 					db::alteration_message("$className : skipped");
 				}
 				else {
@@ -58,7 +60,43 @@ class DataIntegrityTestYML extends BuildTask {
 					else {
 						db::alteration_message("$className", "created");
 						foreach($variables as $variable => $setting) {
-							if(in_array($variable, $variablesToSkip)) {
+							if($variable == "icon") {
+								$fileLocationForOthers = Director::baseFolder().'/'.$setting;
+								$fileLocationForSiteTree = Director::baseFolder().'/'.$setting.'-file.gif';
+								if( $className::create() instanceof SiteTree) {
+									if(!file_exists($fileLocationForSiteTree)) {
+										db::alteration_message("&nbsp; &nbsp; &nbsp; <u>$className.$variable</u> icon $fileLocationForSiteTree can not be found", "deleted");
+									}
+									else {
+										db::alteration_message("&nbsp; &nbsp; &nbsp; <u>$className.$variable</u> icon $fileLocationForSiteTree exists", "created");
+									}
+								}
+								else {
+									if(!file_exists($fileLocationForOthers)) {
+										db::alteration_message("&nbsp; &nbsp; &nbsp; <u>$className.$variable</u> icon $fileLocationForOthers can not be found", "deleted");
+									}
+									else {
+										db::alteration_message("&nbsp; &nbsp; &nbsp; <u>$className.$variable</u> icon $fileLocationForOthers exists", "created");
+									}
+
+								}
+							}
+							elseif($variable == "extensions") {
+								if(!is_array($setting)) {
+									db::alteration_message("&nbsp; &nbsp; &nbsp; <u>$className.$variable</u> extensions should be set as an array.", "deleted");
+								}
+								else {
+									foreach($setting as $extensionClassName) {
+										if(!class_exists($extensionClassName)){
+											db::alteration_message("&nbsp; &nbsp; &nbsp; <u>$className.$variable</u> extension class <u>$extensionClassName</u> does not exist", "deleted");
+										}
+										else {
+											db::alteration_message("&nbsp; &nbsp; &nbsp; <u>$className.$variable</u> extension class <u>$extensionClassName</u> found", "created");
+										}
+									}
+								}
+							}
+							elseif(in_array($variable, $variablesToSkip)) {
 								db::alteration_message("&nbsp; &nbsp; &nbsp; <u>$className.$variable</u> skipped");
 							}
 							else {
