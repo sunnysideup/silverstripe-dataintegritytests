20695353bfabe9f04d6119273be9f7f59d8570a8
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2014-11-18 03:58:22 +0000

    adding innodb tool
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/dataintegritytests/trunk@5890 7b40dde9-fc51-43d4-b609-63a66f0a0cee

---
+++ code/DataIntegrityTestInnoDB.php
@@ -0,0 +1,77 @@
+<?php
+
+
+class DataIntegrityTestInnoDB extends BuildTask {
+
+	/**
+	 * standard SS variable
+	 * @var String
+	 */
+	protected $title = "Convert all tables to InnoDB.";
+
+	/**
+	 * standard SS variable
+	 * @var String
+	 */
+	protected $description = "Converts table to innoDB. CAREFUL: replaces all tables in Database to innoDB - not just the Silverstripe ones.";
+
+	function run($request) {
+		ini_set('max_execution_time', 3000);
+		$tables = DB::query('SHOW tables');
+		foreach ($tables as $table) {
+			$table = array_pop($table);
+			DB::alteration_message("Updating $table to innoDB", "created");$this->flushNow();
+			$indexRows = DB::query("SHOW INDEX FROM \"$table\" WHERE Index_type = 'FULLTEXT'");
+			unset($done);$done = array();
+			foreach($indexRows as $indexRow) {
+				$key = $indexRow["Key_name"];
+				if(!isset($done[$key])) {
+					DB::alteration_message("Deleting INDEX $key in $table (FullText Index)", "deleted");$this->flushNow();
+					DB::query("ALTER TABLE \"$table\" DROP INDEX $key;");
+					$done[$key] = $key;
+				}
+			}
+			$sql = "ALTER TABLE \"$table\" ENGINE=INNODB";
+			DB::query($sql);
+		}
+		$rows = DB::query("SHOW GLOBAL STATUS LIKE  'Innodb_page_size'");
+		foreach($rows as $row) {
+			$currentInnoDBSetting = $row["Value"];
+		}
+		$innoDBBufferUsed = DB::query("
+
+SELECT (PagesData*PageSize)/POWER(1024,3) DataGB FROM
+(SELECT variable_value PagesData
+FROM information_schema.global_status
+WHERE variable_name='Innodb_buffer_pool_pages_data') A,
+(SELECT variable_value PageSize
+FROM information_schema.global_status
+WHERE variable_name='Innodb_page_size') B;
+
+		")->value();
+		$innoBDBufferRecommended = DB::query( "
+SELECT CEILING(Total_InnoDB_Bytes*1.6/POWER(1024,3)) RIBPS FROM
+ (SELECT SUM(data_length+index_length) Total_InnoDB_Bytes
+ FROM information_schema.tables WHERE engine='InnoDB') A;
+"
+		)->value();
+		DB::alteration_message("<hr /><hr /><hr /><hr /><hr /><hr /><hr />COMPLETED
+		<br />
+		Please check your MYSQL innodb_buffer_pool_size setting.
+		It is currently using $innoDBBufferUsed GIGABYTES,
+		but it should be set to $innoBDBufferRecommended GIGABYTES.
+		The current setting is: ".($currentInnoDBSetting / (1042 * 1024))."
+		<hr /><hr /><hr /><hr /><hr /><hr /><hr />");
+	}
+
+	private function flushNow(){
+		// check that buffer is actually set before flushing
+		if (ob_get_length()){
+			@ob_flush();
+			@flush();
+			@ob_end_flush();
+		}
+		@ob_start();
+	}
+
+}
