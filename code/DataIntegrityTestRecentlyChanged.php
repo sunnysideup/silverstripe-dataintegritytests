edb9f6825fcf5a041242ed9a9057edde648096e9
Sunny Side Up <github@sunnysideup.co.nz>
2016-01-19 16:40:26 +1300

    BUGFIXES: fixes in DataIntegrityTestRecentlyChanged

--- code/DataIntegrityTestRecentlyChanged.php
+++ code/DataIntegrityTestRecentlyChanged.php
@@ -14,8 +14,7 @@ class DataIntegrityTestRecentlyChanged extends BuildTask {
 	 * standard SS variable
 	 * @var String
 	 */
-	protected $description = "Go through all tables in the database and see what records have been edited in the last xxx minutes.  You can set the minutes using a GET variable (http://www.mysite.co.nz/dev/tasks/DataIntegrityTestRecentlyChanged/?x=123 where 123 is the number of minutes).";
-
+	protected $description = "Go through all tables in the database and see what records have been edited in the last xxx minutes.  You can set the minutes using a GET variable (http://www.sunnysideup.co.nz/dev/tasks/DataIntegrityTestRecentlyChanged/?x=123 where 123 is the number of minutes).";
 
 	function run($request) {
 		ini_set('max_execution_time', 3000);
@@ -25,35 +24,47 @@ class DataIntegrityTestRecentlyChanged extends BuildTask {
 			$dataClasses = ClassInfo::subclassesFor('DataObject');
 			array_shift($dataClasses);
 			foreach($dataClasses as $dataClass) {
-				// Check if class exists before trying to instantiate - this sidesteps any manifest weirdness
-				if(class_exists($dataClass) && $this->tableExists($dataClass)) {
-					$whereStatement = $whereStatementFixed." AND \"ClassName\" = '".$dataClass."' ";
+				if(class_exists($dataClass)) {
 					$singleton = Injector::inst()->get($dataClass);
-					$count = $dataClass::get()->where($whereStatement)->count();
-					$fields = Config::inst()->get($dataClass, "db", Config::INHERITED);
-					if(!is_array($fields)) {
-						$fields = array();
+					if($singleton instanceof TestOnly) {
+						//do nothing
 					}
-					$fields = array("ID" => "Int", "Created" => "SS_DateAndTime", "LastEdited" => "SS_DateAndTime")+$fields;
-					if($count) {
-						echo "<h2>".$singleton->singular_name()."(".$count.")</h2><table><thead>";
-						echo "<tr><th>".implode("</th><th>",array_keys($fields))."</th></tr></thead><tbody>";
-						$objects = $dataClass::get()->where($whereStatement)->limit(1000);
-						foreach($objects as $object) {
-							if(count($fields)) {
-								$array = array();
-								foreach($fields as $field => $typeOfField) {
-									$array[] = substr(strip_tags($object->$field), 0, 100);
-								}
-								echo "<tr><td>".implode("</td><td>", $array)."</td></li>";
-							}
+					else {
+						$whereStatement = $whereStatementFixed." AND \"ClassName\" = '".$dataClass."' ";
+						$count = $dataClass::get()->where($whereStatement)->count();
+						$fields = Config::inst()->get($dataClass, "db", Config::INHERITED);
+						if(!is_array($fields)) {
+							$fields = array();
 						}
-						echo "</tbody></table>";
+						$fields = array("ID" => "Int", "Created" => "SS_DateAndTime", "LastEdited" => "SS_DateAndTime")+$fields;
+						if($count) {
+							echo "<h2>".$singleton->singular_name()."(".$count.")</h2><table><thead>";
+							echo "<tr><th>".implode("</th><th>",array_keys($fields))."</th></tr></thead><tbody>";
+							$objects = $dataClass::get()->where($whereStatement)->limit(1000);
+							foreach($objects as $object) {
+								if(count($fields)) {
+									$array = array();
+									foreach($fields as $field => $typeOfField) {
+										$array[] = substr(strip_tags($object->$field), 0, 1000);
+									}
+									echo "<tr><td>".implode("</td><td>", $array)."</td></li>";
+								}
+							}
+							echo "</tbody></table>";
+						}
 					}
 				}
 			}
+			echo "<h1>-------- END --------</h1>";
 		}
-		echo "<h1>-------- END --------</h1>";
+		if(empty($_GET["m"])) {
+			$_GET["m"] = 0;
+		}
+		echo "
+			<form method=\"get\" action=\"".Director::absoluteURL("dev/tasks/".$this->class."/")."\">
+				<label for=\"m\">minutes ago</label>
+				<input name=\"m\" id=\"m\" value=\"".$_GET["m"]."\">
+			</form>";
 	}
 
 
