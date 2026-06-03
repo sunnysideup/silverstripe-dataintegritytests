df0ea7f868f2f054dbc487a6c9008f8f487f30b2
Nicolaas @ Tappy <nt@sunnysideup.co.nz>
2026-06-03 14:29:19 +1200

    FIX: cleanup

--- src/DataIntegrityTestRecentlyChanged.php
+++ src/DataIntegrityTestRecentlyChanged.php
@@ -87,16 +87,16 @@ class DataIntegrityTestRecentlyChanged extends BuildTask
 
             echo '<hr /><h1>-------- THE END --------</h1>';
         }
-
-        if (empty($_GET['m'])) {
-            $_GET['m'] = 0;
+        $minutes = (int) $request->getVar('m');
+        if (! $minutes) {
+            $minutes = 3600;
         }
 
         echo '
 
 			<form method="get" action="' . Director::absoluteURL('dev/tasks/' . $this->Config()->get('segment') . '/') . '">
 				<label for="m">please enter minutes ago or any date (e.g. last week, yesterday, 2011-11-11, etc...)</label>
-				<input name="m" id="m" value="' . $_GET['m'] . '">
+				<input name="m" id="m" value="' . $minutes . '">
 			</form>';
     }
 
